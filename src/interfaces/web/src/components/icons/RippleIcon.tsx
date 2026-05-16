fd87fcec30e07e8cd1aad77ebfae1d1dc23f053f
lake <echonoshy@gmail.com>
2026-05-16 15:10:23 +0800

    Keep final Ripple icon only

--- src/interfaces/web/src/components/icons/RippleIcon.tsx
+++ src/interfaces/web/src/components/icons/RippleIcon.tsx
@@ -6,7 +6,7 @@ interface RippleIconProps {
 export default function RippleIcon({ size = 24, className = "" }: RippleIconProps) {
   return (
     <img
-      src="/ripple-icon.svg?v=20260516-r8"
+      src="/ripple-icon.svg?v=20260516-r9"
       alt=""
       aria-hidden="true"
       width={size}
